d885bfc9a0b79f321facb96f96d71cde435ee4fa
kunlekerx <habibajibola02@gmail.com>
2025-05-18 22:45:10 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>React Debugging Demo</h1>
+      <Counter initialCount={5} />
+    </div>
+  );
+}
+
+export default App;
